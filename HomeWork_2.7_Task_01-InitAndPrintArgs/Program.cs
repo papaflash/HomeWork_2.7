bec1428295de4e58cf3380e6003f9269d26733a2
Alexander <shagov25@gmail.com>
2022-07-07 15:04:40 +0300

    Добавьте файлы проекта.

---
+++ HomeWork_2.7_Task_01-InitAndPrintArgs/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HomeWork_2._7_Task_01_InitAndPrintArgs
+{
+    internal class Program
+    {
+        /* TODO:
+            Ф. И. О.
+            Возраст
+            Email
+            Баллы по программированию
+            Баллы по математике
+            Баллы по физике
+         */
+        static void Main(string[] args)
+        {
+            string _fullName = "Shagov Alexander Valerevich";
+            int _age = 36;
+            string _email = "shagov25@gmail.com";
+            double _programmingScores = 90.56;
+            double _mathScores = 80.78;
+            double _physicsScores = 75.0;
+
+            Console.WriteLine($"Ф.И.О. - {_fullName}");
+            Console.WriteLine($"Возраст - {_age}");
+            Console.WriteLine($"Адрес эл. Почты - {_email}");
+            Console.WriteLine($"Баллы по программированию - {_programmingScores:(#)} Балл");
+            Console.WriteLine($"Баллы по математике - {_mathScores:(#)} Балл");
+            Console.WriteLine($"Баллы по физике - {_physicsScores:(#)} Баллов");
+
+            Console.ReadKey();
+        }
+    }
+}
